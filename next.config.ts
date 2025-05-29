65da6d7c43f4307f056436c85ead33c4174eba6d
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2025-05-29 00:03:55 -0700

    increased bodySizeLimit

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         pathname: '/storage/v1/object/public/images/**',
       },
     ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
   },
 };
 
