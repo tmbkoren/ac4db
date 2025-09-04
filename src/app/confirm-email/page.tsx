3a11523de13d8870dd73c19cb5a99c386c3533fa
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2025-09-04 12:28:49 -0700

    lint fix

--- src/app/confirm-email/page.tsx
+++ src/app/confirm-email/page.tsx
@@ -6,7 +6,7 @@ export default function ConfirmEmailPage() {
     <Container size="xs" style={{ marginTop: '2rem', textAlign: 'center' }}>
       <Title order={2}>Check your email</Title>
       <Text mt="md">
-        We've sent a verification link to your email address. Please click the link to complete your registration.
+        We&apos;ve sent a verification link to your email address. Please click the link to complete your registration.
       </Text>
     </Container>
   );
