3ec621a63c42cfd3a29a7db0541e37ff6e438c56
Vladyslav Korenevskyi <25588519+tmbkoren@users.noreply.github.com>
2025-09-04 17:43:34 -0700

    fix2

--- src/app/confirm-email/page.tsx
+++ src/app/confirm-email/page.tsx
@@ -1,21 +1,20 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
 import { confirmEmail } from '@/app/confirm-email/actions';
 
-export default function ConfirmEmailPage({
-  searchParams,
-}: {
-  searchParams: { token_hash: string; type: string };
-}) {
+export default function ConfirmEmailPage() {
+  const searchParams = useSearchParams();
+  const token_hash = searchParams.get('token_hash');
+  const type = searchParams.get('type');
+
   return (
     <div>
       <h1>Confirm your email</h1>
       <p>Click the button below to confirm your email address.</p>
       <form action={confirmEmail}>
-        <input
-          type="hidden"
-          name="token_hash"
-          value={searchParams.token_hash}
-        />
-        <input type="hidden" name="type" value={searchParams.type} />
+        <input type="hidden" name="token_hash" value={token_hash ?? ''} />
+        <input type="hidden" name="type" value={type ?? ''} />
         <button type="submit">Confirm Email</button>
       </form>
     </div>
